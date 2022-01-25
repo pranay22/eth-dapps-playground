1a819cc06095b2a476c67a7c8031617362ebc032
Pranay Sarkar <pranaysarkar22@gmail.com>
2022-01-25 15:16:58 +0100

    na: erc-20 interface [Pranay Sarkar] [V]

---
+++ erc-20_token_1/ERC20Interface.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.4.24;
+
+contract ERC20Interface {
+
+    string public constant name = "Pranay Token";
+    string public constant symbol = "PRT";
+    uint8 public constant decimals = 18;  // 18 is the most common number of decimal places
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+
+}
+
